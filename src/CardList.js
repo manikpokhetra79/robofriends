a3f80883484f540a4f2adb31e9c69b089d70b69f
Manik Gupta <manikpokhetra@gmail.com>
2021-09-28 21:39:05 +0530

    created searchbox component

--- src/CardList.js
+++ src/CardList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Card from './Card';
-import { robots } from './robots';
-const CardList = () => {
+
+const CardList = ({ robots }) => {
   return (
     <>
       {robots.map((robot, index) => (
