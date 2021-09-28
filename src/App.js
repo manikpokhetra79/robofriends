d1f5e4826255bec8dd794c3adf24b04133a5c5c3
Manik Gupta <manikpokhetra@gmail.com>
2021-09-28 22:08:28 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,35 @@ import React from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor() {
+    super(); //calls constructor of this component
+    this.state = {
+      robots: robots,
+      searchfield: '',
+    };
+  }
+  onSearchChange = (e) => {
+    this.setState({
+      searchfield: e.target.value,
+    });
+  };
+  render() {
+    const filteredRobots = this.state.robots.filter((robots) => {
+      //this will return only the robots whose name is same as searched input
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    const robots = this.state.robots;
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
 
 export default App;
